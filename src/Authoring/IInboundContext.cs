aec5147504b962e862dd585167b400b44432e357
Rafal Mielowski <rmielowski@microsoft.com>
2024-08-09 14:55:29 +0200

    Add example of inline documentation

--- src/Authoring/IInboundContext.cs
+++ src/Authoring/IInboundContext.cs
@@ -4,10 +4,50 @@ namespace Mielek.Azure.ApiManagement.PolicyToolkit.Authoring;
 
 public interface IInboundContext : IHaveExpressionContext
 {
+    /// <summary>
+    /// Adds header of specified name with values or overrides values if header already exists.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set. Policy expressions are allowed.
+    /// </param>
     void SetHeader(string name, params string[] values);
+
+    /// <summary>
+    /// Sets header of specified name and values if header not already exist.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set. Policy expressions are allowed.
+    /// </param>
     void SetHeaderIfNotExist(string name, params string[] values);
+
+    /// <summary>
+    /// Adds header of specified name with values or appends values if header already exists.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set or appended. Policy expressions are allowed.
+    /// </param>
     void AppendHeader(string name, params string[] values);
+
+    /// <summary>
+    /// Deletes header of specified name.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be deleted. Policy expressions are allowed.
+    /// </param>
     void RemoveHeader(string name);
+
+    /// <summary>
+    /// The base policy used to specify when parent scope policy should be executed
+    /// </summary>
     void Base();
 
     string AuthenticationManagedIdentity(string resource);
@@ -31,4 +71,12 @@ public interface IInboundContext : IHaveExpressionContext
     void RateLimitByKey();
     void SetMethod();
     void SetQueryParameter();
+
+    /// <summary>
+    /// Inlines the specified policy as is to policy document.
+    /// </summary>
+    /// <param name="policy">
+    /// Policy in xml format.
+    /// </param>
+    void InlinePolicy(string policy);
 }
