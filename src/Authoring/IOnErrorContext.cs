91f72b08d2e0ed291c489c6a9763bd3ef7a779c6
Rafal Mielowski <rmielowski@microsoft.com>
2024-10-11 17:43:15 +0200

    Add set header and base to on error context

--- src/Authoring/IOnErrorContext.cs
+++ src/Authoring/IOnErrorContext.cs
@@ -5,6 +5,56 @@ public interface IOnErrorContext : IHaveExpressionContext
     /// <summary>
     /// TODO
     /// </summary>
+    void Base();
+
+    /// <summary>
+    /// Adds header of specified name with values or overrides values if header already exists.<br />
+    /// Compiled to <a href="https://learn.microsoft.com/en-us/azure/api-management/set-header-policy">set-header</a> policy.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set. Policy expressions are allowed.
+    /// </param>
+    void SetHeader(string name, params string[] values);
+
+    /// <summary>
+    /// Sets header of specified name and values if header not already exist.<br />
+    /// Compiled to <a href="https://learn.microsoft.com/en-us/azure/api-management/set-header-policy">set-header</a> policy.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set. Policy expressions are allowed.
+    /// </param>
+    void SetHeaderIfNotExist(string name, params string[] values);
+
+    /// <summary>
+    /// Adds header of specified name with values or appends values if header already exists.<br />
+    /// Compiled to <a href="https://learn.microsoft.com/en-us/azure/api-management/set-header-policy">set-header</a> policy.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be added. Policy expressions are allowed.
+    /// </param>
+    /// <param name="values">
+    /// Specifies the values of the header to be set or appended. Policy expressions are allowed.
+    /// </param>
+    void AppendHeader(string name, params string[] values);
+
+    /// <summary>
+    /// Deletes header of specified name.<br />
+    /// Compiled to <a href="https://learn.microsoft.com/en-us/azure/api-management/set-header-policy">set-header</a> policy.
+    /// </summary>
+    /// <param name="name">
+    /// Specifies name of the header to be deleted. Policy expressions are allowed.
+    /// </param>
+    void RemoveHeader(string name);
+    
+    /// <summary>
+    /// TODO
+    /// </summary>
     /// <param name="name"></param>
     /// <param name="value"></param>
     void SetVariable(string name, dynamic value);
