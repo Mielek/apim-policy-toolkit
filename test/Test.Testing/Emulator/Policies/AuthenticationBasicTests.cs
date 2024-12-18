60a3a99ca8e149200d233b74a88a418a331ef0fd
Rafal Mielowski <rmielowski@microsoft.com>
2024-12-18 18:09:10 +0100

    wip

---
+++ test/Test.Testing/Emulator/Policies/AuthenticationBasicTests.cs
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Azure.ApiManagement.PolicyToolkit.Authoring;
+using Azure.ApiManagement.PolicyToolkit.Authoring.Expressions;
+using Azure.ApiManagement.PolicyToolkit.Testing;
+using Azure.ApiManagement.PolicyToolkit.Testing.Document;
+
+namespace Test.Emulator.Emulator.Policies;
+
+[TestClass]
+public class AuthenticationBasicTests
+{
+    class SimpleABasic : IDocument
+    {
+        public void Inbound(IInboundContext context)
+        {
+            context.AuthenticationBasic("test", "tset");
+        }
+    }
+
+    class MultipleABasic : IDocument
+    {
+        public void Inbound(IInboundContext context)
+        {
+            context.AuthenticationBasic("a", "a-pass");
+            context.AuthenticationBasic("b", "b-pass");
+        }
+    }
+
+    [TestMethod]
+    public void AuthenticationBasic_HandleSimple()
+    {
+        // Arrange
+        var test = new SimpleABasic().AsTestDocument();
+
+        // Act
+        test.RunInbound();
+
+        // Assert
+        var authHeader = test.Context.Request.Headers.GetValueOrDefault("Authorization");
+        authHeader.Should().NotBeNullOrEmpty().And.StartWith("Basic ");
+        authHeader.TryParseBasic(out var credentials).Should().BeTrue();
+        credentials.Should().NotBeNull();
+        credentials!.Username.Should().Be("test");
+        credentials.Password.Should().Be("tset");
+    }
+
+    [TestMethod]
+    public void AuthenticationBasic_HandleCallback()
+    {
+        // Arrange
+        var test = new SimpleABasic().AsTestDocument();
+        test.InInbound().AuthenticationBasic().WithCallback((context, user, pass) =>
+        {
+            context.Request.Headers["Authorization"] = [$"Basic {user}:{pass}"];
+        });
+
+        // Act
+        test.RunInbound();
+
+        // Assert
+        var authHeader = test.Context.Request.Headers.GetValueOrDefault("Authorization");
+        authHeader.Should().NotBeNullOrEmpty().And.StartWith("Basic ").And.EndWith("test:tset");
+    }
+
+    [TestMethod]
+    public void AuthenticationBasic_HandleCallback_WithPredicate()
+    {
+        // Arrange
+        var test = new MultipleABasic().AsTestDocument();
+        test.InInbound()
+            .AuthenticationBasic((_, user, pass) => user == "b" && pass == "b-pass")
+            .WithCallback((context, user, pass) =>
+            {
+                context.Request.Headers["B"] = [$"{user}:{pass}"];
+            });
+        test.InInbound()
+            .AuthenticationBasic((_, user, pass) => user == "a" && pass == "a-pass")
+            .WithCallback((context, user, pass) =>
+            {
+                context.Request.Headers["A"] = [$"{user}:{pass}"];
+            });
+
+        // Act
+        test.RunInbound();
+
+        // Assert
+        test.Context.Request
+            .Headers.Should().ContainKey("A")
+            .WhoseValue.Should().ContainSingle()
+            .Which.Should().Be("a:a-pass");
+        test.Context.Request
+            .Headers.Should().ContainKey("B")
+            .WhoseValue.Should().ContainSingle()
+            .Which.Should().Be("b:b-pass");
+    }
+}
